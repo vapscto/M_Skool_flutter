98d9faabdd04b83ff0c0d67b7faa55fdb8d7593f
Ankush <ankushvarma262@gmail.com>
2023-01-04 13:03:39 +0530

    pull

---
+++ lib/staffs/notice_board_staff/screen/notice_board_staff_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoticeBoardStaffHome extends StatefulWidget {
+  const NoticeBoardStaffHome({super.key});
+
+  @override
+  State<NoticeBoardStaffHome> createState() => _NoticeBoardStaffHomeState();
+}
+
+class _NoticeBoardStaffHomeState extends State<NoticeBoardStaffHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
