3cda18e52ac229334b1056cfd7a81e7fcfd662d7
Ankush <ankushvarma262@gmail.com>
2023-01-06 16:58:43 +0530

    ..

---
+++ lib/manager/student_notice_board/screen/student_notice_board_home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class StudentNoticeBoardHome extends StatefulWidget {
+  const StudentNoticeBoardHome({super.key});
+
+  @override
+  State<StudentNoticeBoardHome> createState() => _StudentNoticeBoardHomeState();
+}
+
+class _StudentNoticeBoardHomeState extends State<StudentNoticeBoardHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
